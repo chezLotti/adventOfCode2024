c948580329d56be983b5e5a64d36acf15ca5c3d1
chezLotti <charlottelhaw@gmail.com>
2024-12-02 22:03:26 +0100

    star 1 at least

--- src/day02/DayTwo.hs
+++ src/day02/DayTwo.hs
@@ -1,15 +1,50 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module DayTwo (
 
 ) where
 
--- star 1
-getDistances :: [Int] -> [(Int, Int)]
-getDistances xs = [(abs $ x - y, signum $ x - y) | (x, y) <- zip xs (tail xs)]
+inputFile :: String
+inputFile = "/home/charlotte/projects/haskell/adventOfCode2024/src/day02/input.txt"
 
-checkIfSafe :: [(a, Int)] -> Bool
-checkIfSafe xs = (length xs) == (abs $ sum [snd x | x <- xs])
+handleInput :: ([[Int]] -> b) -> FilePath -> IO b
+handleInput f filePath = do
+  contents <- readFile filePath
+  let lineContents = lines contents
+  let entriesPerLine = [map read (words x) :: [Int] | x <- lineContents]
+  return (f entriesPerLine)
+
+{-
+second :: (a, b, c) -> b
+second (_, x, _) = x
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+third :: (a, b, c) -> c
+third (_, _, x) = x
+-}
+
+-- star 1
+getDistances :: Num b => [b] -> [(b, b)]
+getDistances xs =
+  [(abs $ x - y, signum $ x - y) | (x, y) <- zip xs (tail xs)]
+
+checkIfAscOrDsc :: [(a, Int)] -> Bool
+checkIfAscOrDsc xs = length xs == abs (sum [snd x | x <- xs])
+
+checkDistances :: (Eq a, Num a) => [(a, b)] -> Bool
+checkDistances xs
+  | null xs = True
+  | x /= 1 && x /= 2 && x /= 3 = False
+  | x == 1 || x == 2 || x == 3 = checkDistances (tail xs)
+  | otherwise = True
+  where
+    x = fst $ head xs
 
 countSafeReports :: [[Int]] -> Int
-countSafeReports xs = length [1 | x <- xs, checkIfSafe (getDistances x)]
+countSafeReports xs =
+  length [x | x <- xs
+            , let ys = getDistances x
+            , checkIfAscOrDsc ys && checkDistances ys]
 
 -- star 2
